ea0f9ac56e78dd7fc7f68eb14cf0924d538f3f2d
Daniel Wikander <daniel.wikander@gmail.com>
2020-05-14 19:35:21 +0200

    update

---
+++ benchmarks/koa-REST.js
@@ -0,0 +1,12 @@
+
+const Koa = require("koa");
+const { data } = require("../lib/data");
+const md5 = require("md5");
+
+const app = new Koa()
+  .use((ctx) => {
+    ctx.body = data.map(x => ({ ...x, md5: md5(x.name) }));
+  });
+
+
+app.listen(4001);
--- benchmarks/yoga-graphql.js
+++
@@ -1,7 +0,0 @@
-const { GraphQLServer } = require("graphql-yoga");
-const { createApolloSchema } = require("../lib/createApolloSchema");
-const schema = createApolloSchema();
-
-const server = new GraphQLServer({ schema });
-
-server.start({ port: 4001, endpoint: "/graphql" });
